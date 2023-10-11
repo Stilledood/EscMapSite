0ab0e401424736a4b8c4d37d2d6756fca8b8e610
Florin C. <corciova.f@gmail.com>
2023-10-11 17:41:07 +0300

    Created services page

--- EscMap/EscMap/urls.py
+++ EscMap/EscMap/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='homepage.html'))
+    path('',TemplateView.as_view(template_name='homepage.html')),
+    path('services/social_media_management', TemplateView.as_view(template_name='social_media_management.html'))
 ]

1fa414eed3506239826b55b69f1c5a396b2e4706
Florin C. <corciova.f@gmail.com>
2023-10-19 17:31:42 +0300

    Added doodle css 5

--- EscMap/EscMap/urls.py
+++ EscMap/EscMap/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='homepage.html')),
-    path('services/social_media_management', TemplateView.as_view(template_name='social_media_management.html'))
+    path('services/social_media_management', TemplateView.as_view(template_name='social_media_management.html')),
+    path('services/web_development', TemplateView.as_view(template_name="web_development.html"))
 ]
